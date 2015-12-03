efc81f65b436c08f23ae68b31bc2aaf0496e7c70
Anton Todorov <a.todorov@storpool.com>
2015-12-03 14:07:24 +0200

    add fencing script example

---
+++ misc/fencing-script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+
+# -------------------------------------------------------------------------- #
+# Copyright 2015, StorPool (storpool.com)                                    #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+# This script is working with the following environment variables:
+#
+# FT_ACTION={FENCE|THAW}
+# FT_HOSTNAME={kvm node hostname}
+#
+
+PATH=/usr/sbin:/usr/bin:$PATH
+
+[ "`whoami`" = "root" ] && SUDO= || SUDO=sudo
+
+case "$FT_ACTION" in
+    FENCE)
+        $SUDO iptables -I OUTPUT -j REJECT -d "$FT_HOSTNAME"
+        ;;
+    THAW)
+        $SUDO iptables -D OUTPUT -j REJECT -d "$FT_HOSTNAME"
+        ;;
+esac
