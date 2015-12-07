13f35092a27be9cb3919f8a78034ccc8fad81f1c
Anton Todorov <a.todorov@storpool.com>
2015-12-07 11:56:39 +0200

    install.sh: add fencing-script.sh

--- install.sh
+++ install.sh
@@ -162,7 +162,16 @@ chown "$ONE_USER" "$XPATH_MULTI"
 chmod u+x "$XPATH_MULTI"
 
 # install hooks
+echo "*** Install hooks ..."
 cp -v -a hooks/* "$ONE_VAR/remotes/hooks/"
+
+# fencing-script.sh
+if [ -f /usr/sbin/fencing-script.sh ]; then
+    echo "*** File exists: /usr/sbin/fencing-script.sh "
+    echo "*** Please update /usr/sbin/fencing-script.sh using hints from misc/fencing-script.sh"
+else
+    cp -v misc/fencing-script.sh /usr/sbin/
+fi
 
 function patch_hook()
 {
