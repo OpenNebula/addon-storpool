a32c629b64851d40ba6a5aaf8ebc0e5e9da117d6
Anton Todorov <a.todorov@storpool.com>
2016-10-26 14:05:42 +0300

    add live disk resizing POC scripts

---
+++ misc/resize.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+#
+# -------------------------------------------------------------------------- #
+# Copyright 2015-2016, StorPool (storpool.com)                               #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+from sys import exit
+
+try:
+  from lxml import etree
+  #print("running with lxml.etree")
+except ImportError:
+  try:
+    # Python 2.5
+    import xml.etree.cElementTree as etree
+    #print("running with cElementTree on Python 2.5+")
+  except ImportError:
+    try:
+      # Python 2.5
+      import xml.etree.ElementTree as etree
+      #print("running with ElementTree on Python 2.5+")
+    except ImportError:
+      try:
+        # normal cElementTree install
+        import cElementTree as etree
+        #print("running with cElementTree")
+      except ImportError:
+        try:
+          # normal ElementTree install
+          import elementtree.ElementTree as etree
+          #print("running with ElementTree")
+        except ImportError:
+          print("Failed to import ElementTree from any known place")
+
+
+def getEle(etp, name):
+  for e in etp.findall(name):
+    #print "getEle({0}):{1}".format(name,e.text)
+    return e
+
+import argparse
+parser = argparse.ArgumentParser
+parser.add_argument("vmId", help="VM's ID")
+parser.add_argument("diskId", help="VM's disk ID")
+parser.add_argument("diskSize", help="New size in MiB")
+parser.add_argument("dbConn", help="Database connection method details")
+
+args = parser.parse_args
+
+print "VM:{0} DISK:{1} SIZE:{2} DB:{3}".format(args.vmId,args.diskId,args.diskSize,args.dbConn)
+
+dbConn = args.dbConn.split(":")
+if dbConn[0] == 'sqlite3':
+  import sqlite3
+  dbSELECT = "SELECT * FROM ? WHERE oid=?"
+  dbUpdate = "UPDATE ? SET body=? WHERE oid=?"
+  db = sqlite3.connect(dbConn[1])
+elif dbConn[0] == 'mysql':
+  import mysql.connector
+  dbSELECT = "SELECT * FROM %s WHERE oid = %s"
+  dbUpdate = "UPDATE %s SET body = %s WHERE oid = %s"
+  dbconf = {
+     'user': dbConn[1] or 'oneadmin',
+     'password': dbConn[2] or '',
+     'host': dbConn[3] or '127.0.0.1',
+     'database': dbConn[4] or 'opennebula',
+     'raise_on_warnings': True,
+  }
+  db = mysql.connector.connect(**dbconf)
+else:
+  print("Uknown connection type {0}".format(conn[0]))
+  exit(1)
+
+
+dbc = db.cursor()
+
+dbc.execute(dbSELECT, ('vm_pool',args.vmId,))
+
+res = dbc.fetchone()
+
+if res[2]:
+  xmlBody = res[2]
+
+et = etree.XML(xmlBody, etree.XMLParser(strip_cdata=False,remove_blank_text=True))
+
+changed = False
+persistent = None
+
+for disk in et.findall(".//DISK"):
+  disk_size_et = getEle(disk,".//SIZE")
+  disk_id_et = getEle(disk,".//DISK_ID")
+  disk_source_et = getEle(disk,".//SOURCE")
+  disk_imageId_et = getEle(disk,".//IMAGE_ID")
+  disk_clone_et = getEle(disk,".//CLONE")
+  txt = "DISK_ID:{0} SOURCE:{1} SIZE:{2} IMAGE_ID:{3} CLONE:{4}".format(disk_id_et.text,disk_source_et.text,disk_size_et.text,disk_imageId_et.text,disk_clone_et.text)
+  if disk_id_et.text == args.diskId:
+    txt = "{0} <<< size changed to {1}".format(txt,args.diskSize)
+    disk_size_et.text = etree.CDATA(args.diskSize)
+    changed = True
+    if disk_clone_et.text == 'NO':
+      persistent = disk_imageId_et.text
+  print txt
+
+#print etree.tostring(et,pretty_print=True)
+dbc.execute(dbUPDATE,('vm_pool',etree.tostring(et,pretty_print=False),args.vmId,))
+
+if persistent:
+  dbc.execute(dbSELECT,('image_pool',persistent,))
+  res = dbc.fetchone()
+  if res[2]:
+    xmlBody = res[2]
+    img_et = etree.XML(xmlBody, etree.XMLParser(strip_cdata=False,remove_blank_text=True))
+    img_size_et = getEle(img_et,".//SIZE")
+    print "img ID:{0} SIZE:{1} <<< size changed to {2}".format(persistent,img_size_et.text,args.diskSize)
+    img_size_et.text = args.diskSize
+    #print etree.tostring(eti,pretty_print=True)
+    dbc.execute(dbUpdate,('image_pool',etree.tostring(img_et,pretty_print=False),persistent,))
+
+db.commit()
+db.close()
---
+++ misc/resize.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+#
+# -------------------------------------------------------------------------- #
+# Copyright 2015-2016, StorPool (storpool.com)                               #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+VM_ID="$1"
+DISK_ID="$2"
+DISK_SIZE="$3"
+DB="$4"
+
+if [ "$DB" = "" ]; then
+  DB="sqlite3:/var/lib/one/one.db"
+fi
+
+usage()
+{
+  [ -n "$1" ] && echo -e "ee\nee Error: $*\nee\n"
+  echo "$0 <VM_ID> <DISK_ID> <SIZE> <DBconn>"
+  echo
+  echo "Note: the SIZE should be in MiB"
+  echo "DBconn could be eny of"
+  echo "  'sqlite3:/path/to/one.db'"
+  echo "  'mysql:<dbUser>:<dbPassword>:<dbHost>:<dbName>'"
+  echo
+  exit 1
+}
+
+if [ -n "$VM_ID" ]; then
+  if [ -n "${VM_ID//[[:digit:]]/}" ]; then
+    usage "VM_ID '$VM_ID' should be a numer!"
+  fi
+else
+  usage "VM_ID is empty! Please provide VM id"
+fi
+if [ -n "$DISK_ID" ]; then
+  if [ -n "${DISK_ID//[[:digit:]]/}" ]; then
+    usage "DISK_ID '$DISK_ID' should be a numer!"
+  fi
+else
+  usage "DISK_ID is empty! Please provide disk id"
+fi
+if [ -n "$DISK_SIZE" ]; then
+  if [ -n "${DISK_SIZE//[[:digit:]]/}" ]; then
+    usage "SIZE '$DISK_SIZE' should be a numer!"
+  fi
+else
+  usage "SIZE is empty! Please provide the size in MiB"
+fi
+
+DRIVER_PATH="${DRIVER_PATH:-"/var/lib/one/remotes"}"
+
+XPATH="${DRIVER_PATH}/datastore/xpath.rb --stdin"
+
+unset i j XPATH_ELEMENTS
+
+while IFS= read -r -d '' element; do
+    XPATH_ELEMENTS[i++]="$element"
+done < <(onevm show -x $VM_ID |tee vm-${VM_ID}.xml| $XPATH \
+                    /VM/DEPLOY_ID \
+                    /VM/LCM_STATE \
+                    /VM/TEMPLATE/DISK[DISK_ID=$DISK_ID]/SIZE \
+                    /VM/TEMPLATE/DISK[DISK_ID=$DISK_ID]/ORIGINAL_SIZE \
+                    /VM/TEMPLATE/DISK[DISK_ID=$DISK_ID]/SOURCE \
+                    /VM/TEMPLATE/DISK[DISK_ID=$DISK_ID]/CLONE \
+                    /VM/HISTORY_RECORDS/HISTORY[last\(\)]/HOSTNAME)
+
+DEPLOY_ID="${XPATH_ELEMENTS[j++]}"
+LCM_STATE="${XPATH_ELEMENTS[j++]}"
+SIZE="${XPATH_ELEMENTS[j++]}"
+ORIGINAL_SIZE="${XPATH_ELEMENTS[j++]}"
+SOURCE="${XPATH_ELEMENTS[j++]}"
+CLONE="${XPATH_ELEMENTS[j++]}"
+VMHOST="${XPATH_ELEMENTS[j++]}"
+
+if [ -n "$CLONE" ]; then
+  SP_VOL="${SOURCE##*/}"
+  if [ "$CLONE" = "YES" ]; then
+    SP_VOL+="-$VM_ID-$DISK_ID"
+  fi
+else
+  echo "ee Can't get disk info from OpenNebula for DISK_ID:$DISK_ID on VM with ID:$VM_ID!"
+  exit 1
+fi
+if [ -n "$DEBUG" ]; then
+  echo "LCM_STATE:$LCM_STATE SIZE:$SIZE ORIGINAL_SIZE:$SIZE VMHOST:$VMHOST"
+  #echo "CLONE:$CLONE SOURCE:$SOURCE"
+  echo "DEPLOY_ID:$DEPLOY_ID SP_VOL=$SP_VOL"
+fi
+
+# resize the StorPool volume
+echo "ii Resizing $SP_VOL to ${DISK_SIZE}M ..."
+storpool volume "$SP_VOL" size ${DISK_SIZE}M >/dev/null
+ret=$?
+if [ $ret -ne 0 ]; then
+  echo "ee Failed to resize the StorPool volume $SP_VOL (exit status:$ret)"
+  exit $ret
+else
+  echo "ii $SP_VOL resized"
+fi
+
+source "${DRIVER_PATH}/vmm/kvm/kvmrc"
+
+while IFS=',' read -r device drv filename; do
+  device=${device//\"/}
+  drv=${drv//\"/}
+  filename=${filename//\"/}
+  if [ "${filename##*\.}" = "$DISK_ID" ]; then
+    qemu_device="$device"
+    qemu_file="$filename"
+    break
+  fi
+  if [ -n "$DEBUG" ]; then
+    echo "$device | $drv | $filename"
+  fi
+done < <(su - oneadmin -c "ssh $VMHOST \"virsh --connect $LIBVIRT_URI qemu-monitor-command \\\"$DEPLOY_ID\\\"  '{\\\"execute\\\":\\\"query-block\\\"}'\"" 2>/dev/null \
+| jq -r ".return[]|[.device,.inserted.drv,.inserted.image.filename]|@csv")
+
+if [ -n "$qemu_device" ]; then
+  echo "ii Found $qemu_device >>> $device | $drv | $filename"
+  if [ -n "$DEBUG" ]; then
+    set -x
+  fi
+#    su - oneadmin -c "ssh $VMHOST 'virsh --connect $LIBVIRT_URI qemu-monitor-command \"$DEPLOY_ID\" --hmp \"block_resize $qemu_device ${DISK_SIZE}M\"'"
+  su - oneadmin -c "ssh $VMHOST 'virsh --connect $LIBVIRT_URI blockresize \"$DEPLOY_ID\" --path $qemu_file --size ${DISK_SIZE}M'"
+  ret=$?
+  if [ $ret -eq 0 ]; then
+    su - oneadmin -c "ssh $VMHOST 'virsh --connect $LIBVIRT_URI qemu-monitor-command \"$DEPLOY_ID\" --hmp \"info block\"'"
+    su - oneadmin -c "ssh $VMHOST 'virsh --connect $LIBVIRT_URI qemu-monitor-command \"$DEPLOY_ID\" --hmp \"info blockstats\"'"
+  else
+    echo "ww Warning: virsh blockresize $DEPLOY_ID --path $qemu_file --size ${DISK_SIZE}M ($ret)"
+    echo "ww The VM needs a power cycle to recognize the new size"
+  fi
+else
+  echo "ww Can't find the qemu device. Is the VM running?"
+  echo "ww The VM needs a power cycle to recognize the new size"
+fi
+
+echo "ii Stopping opennebula"
+systemctl stop opennebula
+
+slp=5
+echo "ii waiting $slp seconds..."
+sleep $slp
+
+echo "ii Updating the disk size in the database"
+./resize.py "$VM_ID" "$DISK_ID" "$DISK_SIZE" "$DB"
+
+echo "ii Starting opennebula"
+systemctl start opennebula
+
+echo "ii Done."
