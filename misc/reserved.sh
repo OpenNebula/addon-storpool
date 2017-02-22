99b7d9ff71778422e61737608102028a2cec0e9a
Anton Todorov <atodorov-storpool@users.noreply.github.com>
2017-02-22 23:14:25 +0200

    reserved.sh: script to suggest reserved resources
    On systems where cgroups are in use.

---
+++ misc/reserved.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# -------------------------------------------------------------------------- #
+# Copyright 2015-2016, StorPool (storpool.com)                               #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+cgroup="${1:-machine.slice}"
+
+function count_cpus()
+{
+	local _list="$1" _cpu=0 _c= _arr=()
+	for _c in ${_list//,/ }; do
+		_arr=(${_c//-/ })
+		if [ ${#_arr[@]} -eq 2 ]; then
+			_cpu=$((_cpu + _arr[1] - _arr[0]))
+		fi
+		_cpu=$((_cpu + 1))
+	done
+	echo "$_cpu"
+}
+
+mem=($(free -b | grep -i "mem:"))
+cg_arr=($(cgget -v -n -r memory.limit_in_bytes -r cpuset.cpus "$cgroup"))
+root_arr=($(cgget -v -n -r memory.limit_in_bytes -r cpuset.cpus ""))
+cg_cpus=`count_cpus "${cg_arr[1]}"`
+root_cpus=`count_cpus "${root_arr[1]}"`
+
+echo "RESERVED_CPUS=$(((root_cpus-cg_cpus)*100))"
+echo "RESERVED_MEM=$(((mem[1]-cg_arr[0])/1024))"
