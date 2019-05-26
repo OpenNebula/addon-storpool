9dd11986df16c0c11bf55a9c91329590f16ee34e
Anton Todorov <a.todorov@storpool.com>
2019-05-26 13:13:04 +0300

    update install.sh
    Signed-off-by: Anton Todorov <a.todorov@storpool.com>

--- scripts/install-5.8.0.sh
+++ scripts/install-5.8.0.sh
@@ -106,12 +106,14 @@ for MAD in datastore tm; do
     chmod u+x -R "${M_DIR}/storpool"
 done
 
-# install xpath_multi.py
-XPATH_MULTI="$ONE_VAR/remotes/datastore/xpath_multi.py"
-echo "*** Installing ${XPATH_MULTI##*/} ..."
-cp $CP_ARG datastore/xpath_multi.py "$XPATH_MULTI"
-chown "$ONE_USER" "$XPATH_MULTI"
-chmod a+x "$XPATH_MULTI"
+# install xpath_multi.py and xpath-sp.rb
+for f in xpath_multi.py xpath-sp.rb; do
+    XPATH_MULTI="$ONE_VAR/remotes/datastore/$f"
+    echo "*** Installing $f ..."
+    cp $CP_ARG "datastore/$f" "$XPATH_MULTI"
+    chown "$ONE_USER" "$XPATH_MULTI"
+   chmod a+x "$XPATH_MULTI"
+done
 
 echo "*** Clean up old style crontab jobs"
 (crontab -u oneadmin -l | grep -v monitor_helper-sync | crontab -u oneadmin -)
@@ -248,6 +250,7 @@ if [ -n "$EXTRA" ]; then
     echo "*** EXTRA: patch MADs ..."
     pushd "$ONE_LIB"
         do_patch "$CWD/patches/mads/${ONE_VER}/one_hm.rb.patch" "backup"
+        do_patch "$CWD/patches/mads/${ONE_VER}/one_tm.rb.patch" "backup"
     popd
 fi
 
