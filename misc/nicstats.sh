54999b3f9ea832fb9d9797bd04063486ee985f6d
Anton Todorov <atodorov-storpool@users.noreply.github.com>
2019-07-25 18:26:18 +0300

    add misc/nicstats.sh
    Signed-off-by: Anton Todorov <atodorov-storpool@users.noreply.github.com>

---
+++ misc/nicstats.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# -------------------------------------------------------------------------- #
+# Copyright 2015-2019, StorPool (storpool.com)                               #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+#
+
+# To install just drop the file in '/var/lib/one/remotes/im/kvm-probes.d/'
+# and re-sync the hosts with 'su - oneadmin -c "onehost sync --force"'
+#
+# The script will inject per interface network stats in the oned.log
+# The oned log could be regularly parsed and nics stats pushed to an external db
+# for further processing
+
+#27: one-135-0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast master onebr.616 state UNKNOWN mode DEFAULT group default qlen 1000\    link/ether fe:00:70:30:4a:06 brd ff:ff:ff:ff:ff:ff\    RX: bytes  packets  errors  dropped overrun mcast   \    179604395  177435   0       0       0       0       \    TX: bytes  packets  errors  dropped carrier collsns \    135264059  1235572  0       0       0       0       
+poll=
+while read -u 4 l; do
+	a=($l)
+	nic="${a[1]%:}"
+	nica=(${nic//-/ })
+	vid="${nica[1]}"
+	nid="${nica[2]}"
+	if [ "$vid" != "$vidold" ]; then
+		if [ -n "$poll" ]; then
+			echo "VM=[ID=${vidold},POLL=\"$poll\"]"
+		fi
+		poll=
+	fi
+	vidold="$vid"
+	[ -n "$poll" ] && poll+=" "
+	poll+="NIC_STATS=[ID=${nid},RX=${a[28]},TX=${a[41]}]"
+done 4< <(ip -o -s link | grep one-)
+
+if [ -n "$poll" ]; then
+	echo "VM=[ID=${vidold},POLL=\"$poll\"]"
+fi
