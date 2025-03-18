757d31b5112fa307c56ddb96e22748d84c307feb
Anton Todorov <a.todorov@storpool.com>
2025-03-18 10:54:35 +0200

    misc/tagVolumes.sh add support for SP clusters
    Change-Id: I32ed9d82b0eda8189f374af3a08aa32eef34735b

--- misc/tagVolumes.sh
+++ misc/tagVolumes.sh
@@ -28,112 +28,69 @@ ONE_PATH="${ONE_PATH:-/var/lib/one/remotes}"
 
 cd "${ONE_PATH}/tm/storpool"
 
+# shellcheck source=tm/storpool/storpool_common.sh
 source "${ONE_PATH}/tm/storpool/storpool_common.sh"
+
+LOG_PREFIX="misc"
+export LOG_PREFIX
 
 TMP_DIR="$(mktemp -d)"
 trapAdd "rm -rf '${TMP_DIR}'"
 
-vmPool="${TMP_DIR}/vmPool.xml"
-dsPool="${TMP_DIR}/dsPool.xml"
+vmPoolXml="${TMP_DIR}/vmPool.xml"
+dsPoolXml="${TMP_DIR}/dsPool.xml"
+snapshotsJson="${TMP_DIR}/snapshots.json"
 
-${SUDO:-sudo} onevm list -x --extended >"${vmPool}"
-${SUDO:-sudo} onedatastore list -x >"${dsPool}"
+${SUDO:-sudo} onevm list -x --extended >"${vmPoolXml}"
+${SUDO:-sudo} onedatastore list -x >"${dsPoolXml}"
+${SUDO:-sudo} storpool -B -j snapshot list >"${snapshotsJson}"
 
-snapshots_json="${TMP_DIR}/snapshots.json"
-${SUDO:-sudo} storpool -B -j snapshot list >"${snapshots_json}"
+declare -A datastoreSpAuthToken  # datastoreSpAuthToken[DATASTORE_ID]=SP_AUTH_TOKEN
+declare -A datastoreSpApiHttpHost  # datastoreSpApiHttpHost[DATASTORE_ID]=SP_API_HTTP_HOST
+declare -A datastoreSpApiHttpPort  # datastoreSpApiHttpPort[DATASTORE_ID]=SP_API_HTTP_PORT
 
-
-functions volumes_qos
-{
-    unset vols
-    declare -A vols  # vols[VolumeName]=DISK_ID
-    for disk in ${vmDisksMap:-}; do
-        volume="${disk%:*}"
-        vols["${volume}"]="${disk#*:}"  # [VOLUME_NAME]=DISK_ID
-    done
-
-    unset volumesQc
-    declare -A volumesQc  # volumesQc[VOLUME_NAME]=QosClassName
-    for vol in ${vmDisksQcMap:-}; do
-        volumesQc["${vol%%:*}"]="${vol#*:}"  # [VOLUME_NAME]=QOSCLASS
-    done
-
-    declare -A persistentDisksQc  # persistentDisksQc[VOLUME_NAME]=QOSCLASS
-    for vol in ${persistentDisksQcMap:-}; do
-        persistentDisksQc["${vol%%:*}"]="${vol#*:}"  # [VOLUME_NAME]=QOSCLASS
-    done
-
-    declare -A diskType  # diskType[VOLUME_NAME]=DISK_TYPE
-    for vol in ${vmDiskTypeMap:-}; do
-        diskType["${vol%%:*}"]="${vol#*:}"  # [VOLUME_NAME]=DISK_TYPE
-    done
-
-}
-
-declare -A datastoresQc  # datastoresQc[DATASTORE_ID]=QosClassName
-declare -A datastoresSpAuthTokens  # datastoresSpAuthTokens[DATASTORE_ID]=SP_AUTH_TOKEN
-declare -A datastoresSpApiHttpHosts  # datastoresSpApiHttpHosts[DATASTORE_ID]=SP_API_HTTP_HOST
-declare -A datastoresSpApiHttpPorts  # datastoresSpApiHttpPorts[DATASTORE_ID]=SP_API_HTTP_PORT
-
-while read -r -u 4 -d' ' VM_ID; do
-    SP_QOSCLASS=""
+while read -r -u "${vmfd}" VM_ID; do
     vmVolumes=
-    oneVmVolumes "${VM_ID}" "${vmPool}"
-    echo "# VM_ID=${VM_ID} vmVolumes=${vmVolumes}${vmVolumesQc:+, vmVolumesQc=${vmVolumesQc}}"
-    # QoS Begin
-    unset volumesDsIds
-    declare -A volumesDsIds  # volumesDsIds[VOLUME_NAME]=DATASTORE_ID
-    for entry in ${vmDisksDsMap:-}; do
-        volume="${entry%%:*}"
-        dsId="${entry#*:}"
-        if [[ -z "${datastoresQc[${dsId}+found]}" ]] || [[ -z "${datastoresSpAuthTokens[${dsId}+found]}" ]]; then
-            oneDatastoreInfo "${dsId}" "${dsPoolFile}"
-            datastoresQc["${dsId}"]="${DS_SP_QOSCLASS}"  # [DATASTORE_ID]=DS_SP_QOSCLASS
-            if [[ -n "${SP_AUTH_TOKEN}" ]]; then
-                datastoresSpAuthTokens["${dsId}"]="${SP_AUTH_TOKEN}"
-            fi
-            if [[ -n "${SP_API_HTTP_HOST}" ]]; then
-                datastoresSpApiHttpHosts["${dsId}"]="${SP_API_HTTP_HOST}"
-            fi
-            if [[ -n "${SP_API_HTTP_PORT}" ]]; then
-                datastoresSpApiHttpPorts["${dsId}"]="${SP_API_HTTP_PORT}"
-            fi
+    oneVmVolumes "${VM_ID}" "${vmPoolXml}"
+    echo "# VM ${VM_ID} SYSTEM_DS_ID=${VM_DS_ID} vmVolumes=${vmVolumes}"
+
+    if [[ -z "${datastoreSpAuthToken["${VM_DS_ID}"]+found}" ]]; then
+        oneDatastoreInfo "${VM_DS_ID}" "${dsPoolXml}"
+        if [[ -n "${SP_AUTH_TOKEN}" ]]; then
+            datastoreSpAuthToken["${VM_DS_ID}"]="${SP_AUTH_TOKEN}"
         fi
-        volumesDsIds["${volume}"]="${dsId}"  # [VOLUME_NAME]=DATASTORE_ID
-    done
+        if [[ -n "${SP_API_HTTP_HOST}" ]]; then
+            datastoreSpApiHttpHost["${VM_DS_ID}"]="${SP_API_HTTP_HOST}"
+        fi
+        if [[ -n "${SP_API_HTTP_PORT}" ]]; then
+            datastoreSpApiHttpPort["${VM_DS_ID}"]="${SP_API_HTTP_PORT}"
+        fi
+    fi
 
-    unset vols
-    declare -A vols  # vols[VolumeName]=DISK_ID
-    for disk in ${vmDisksMap:-}; do
-        volume="${disk%:*}"
-        vols["${volume}"]="${disk#*:}"  # [VOLUME_NAME]=DISK_ID
-    done
+    if [[ -n "${datastoreSpAuthToken["${VM_DS_ID}"]}" ]]; then
+        export SP_AUTH_TOKEN="${datastoreSpAuthToken["${VM_DS_ID}"]}"
+    else
+        unset SP_AUTH_TOKEN
+    fi
+    if [[ -n "${datastoreSpApiHttpHost["${VM_DS_ID}"]}" ]]; then
+        export SP_API_HTTP_HOST="${datastoreSpApiHttpHost["${VM_DS_ID}"]}"
+    else
+        unset SP_API_HTTP_HOST
+    fi
+    if [[ -n "${datastoreSpApiHttpPort["${VM_DS_ID}"]}" ]]; then
+        export SP_API_HTTP_PORT="${datastoreSpApiHttpPort["${VM_DS_ID}"]}"
+    else
+        unset SP_API_HTTP_PORT
+    fi
 
-    unset volumesQc
-    declare -A volumesQc  # volumesQc[VOLUME_NAME]=QosClassName
-    for vol in ${vmDisksQcMap:-}; do
-        volumesQc["${vol%%:*}"]="${vol#*:}"  # [VOLUME_NAME]=QOSCLASS
-    done
-
-    declare -A persistentDisksQc  # persistentDisksQc[VOLUME_NAME]=QOSCLASS
-    for vol in ${persistentDisksQcMap:-}; do
-        persistentDisksQc["${vol%%:*}"]="${vol#*:}"  # [VOLUME_NAME]=QOSCLASS
-    done
-
-    declare -A diskType  # diskType[VOLUME_NAME]=DISK_TYPE
-    for vol in ${vmDiskTypeMap:-}; do
-        diskType["${vol%%:*}"]="${vol#*:}"  # [VOLUME_NAME]=DISK_TYPE
-    done
-    # QoS End
-
-    for vol in ${vmVolumes}; do
-        if [[ "${vol%iso}" == "${vol}" ]]; then
-            storpoolVolumeTag "${vol}" "one;${LOC_TAG_VAL}:${VM_ID};${VC_POLICY};${SP_QOSCLASS}" "virt;${LOC_TAG:-nloc}:${VM_TAG:-nvm};${VC_POLICY:+vc-policy};${SP_QOSCLASS:+qc}"
-            while read -r -u 5 snap; do
+    for volume in ${vmVolumes}; do
+        if [[ "${volume%iso}" == "${volume}" ]]; then
+            storpoolVolumeTag "${volume}" "one;${LOC_TAG_VAL};${VM_ID};${VC_POLICY}" "virt;${LOC_TAG:-nloc};${VM_TAG:-nvm};${VC_POLICY:+vc-policy}"
+            while read -r -u "${snapfd}" snap; do
                 storpoolSnapshotTag "${snap}" "one;${LOC_TAG_VAL};${VM_ID}" "virt;${LOC_TAG:-nloc};${VM_TAG}"
-            done 5< <( jq -r --arg n "${vol}-ONESNAP" ".data[]|select(.name|startswith(\$n))|.name" "${snapshots_json}" || true)
+            done {snapfd}< <( jq -r --arg name "${volume}-ONESNAP" ".data[]|select(.name|startswith(\$name))|.name" "${snapshotsJson}" || true)
         else
-            echo "# skipping ${vol}"
+            echo "# skipping ${volume}"
         fi
     done
-done 4< <("${ONE_PATH}/datastore/xpath.rb" --stdin %m%/VM_POOL/VM/ID <"${vmPool}" || true)
+done {vmfd}< <(xmlstarlet sel -t -m //VM -v ID -n "${vmPoolXml}" || true)
