45d957c306321e9050d0f3536fac7fddd8887423
Anton Todorov <a.todorov@storpool.com>
2025-06-10 12:26:51 +0300

    bump copyright
    Change-Id: Ifb7279b680abe2e1a02028f01d386ac3ba82b4bd

--- datastore/xpath_multi.py
+++ datastore/xpath_multi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 # -------------------------------------------------------------------------- #
-# Copyright 2015-2024, StorPool (storpool.com)                               #
+# Copyright 2015-2025, StorPool (storpool.com)                               #
 #                                                                            #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may    #
 # not use this file except in compliance with the License. You may obtain    #
