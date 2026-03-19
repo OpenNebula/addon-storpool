04c8ae4d0bff3b6a443638595561f363395f98d5
Anton Todorov <a.todorov@storpool.com>
2026-03-19 17:42:19 +0200

    misc/reserved.sh: refactor to work with cgroups v2
    Change-Id: Id6ab73eb50062069c87488f10e2ea7d14d146c82
    Signed-off-by: Anton Todorov <a.todorov@storpool.com>

--- misc/reserved.sh
+++ misc/reserved.sh
@@ -19,6 +19,12 @@
 PATH="/bin:/usr/bin:/sbin:/usr/sbin:${PATH}"
 
 cgroup="${1:-machine.slice}"
+cgpath="/sys/fs/cgroup"
+
+if [[ -f /var/tmp/one/addon-storpoolrc ]]; then
+    # shellcheck source=addon-storpoolrc
+    source /var/tmp/one/addon-storpoolrc
+fi
 
 function count_cpus()
 {
@@ -75,24 +81,31 @@ function exclude_cpus()
 }
 
 read -r -a mem_arr <<< "$(free -b | grep -i "mem:" || true)"
-read -r -a cg_arr <<< "$(cgget -v -n -r memory.limit_in_bytes -r cpuset.cpus "${cgroup}" | tr '\n' ' ' || true)"
-read -r -a root_arr <<< "$(cgget -v -n -r memory.limit_in_bytes -r cpuset.cpus "" | tr '\n' ' ' || true)"
-cg_cpuset="${cg_arr[1]}"
-root_cpuset="${root_arr[1]}"
+root_mem="${mem_arr[1]}"
+
+read -r -a cgmount <<<"$(grep cgroup /proc/mounts)"
+#cgroup2 /sys/fs/cgroup cgroup2 rw,nosuid,nodev,noexec,relatime,nsdelegate,memory_recursiveprot 0 0
+if [[ "${cgmount[2]}" == "cgroup2" ]]; then
+    cgver="2"
+    cg_mem="$(<"${cgpath}/${cgroup}/memory.max")"
+    cg_cpuset="$(<"${cgpath}/${cgroup}/cpuset.cpus.effective")"
+    root_cpuset="$(<"${cgpath}/cpuset.cpus.effective")"
+else
+    cgver="1"
+    cg_mem="$(<"${cgpath}/memory/${cgroup}/memory.limit_in_bytes")"
+    cg_cpuset="$(<"${cgpath}/cpuset/${cgroup}/cpuset.cpus")"
+    root_cpuset="$(<"${cgpath}/cpuset/cpuset.cpus")"
+fi
+
 cg_cpus="$(count_cpus "${cg_cpuset}")"
 root_cpus="$(count_cpus "${root_cpuset}")"
 
-if [[ -f /var/tmp/one/addon-storpoolrc ]]; then
-    # shellcheck source=/dev/null
-    source /var/tmp/one/addon-storpoolrc
-fi
-
 [[ -n "${SKIP_RESERVED_CPU}" ]] || echo "RESERVED_CPU=$(((root_cpus-cg_cpus)*100))"
 
-echo "RESERVED_MEM=$(((mem_arr[1]-cg_arr[0])/1024))"
+echo "RESERVED_MEM=$(( (root_mem - cg_mem) / 1024 ))"
 
 storpool_confshow SP_OURID 2>/dev/null
 
 isolcpus=$(exclude_cpus "${root_cpuset}" "${cg_cpuset}")
-[[ -z ${DEBUG:-} ]] || echo "root_cpuset '${root_cpuset}', cg_cpuset '${cg_cpuset}', isolcpus '${isolcpus}'" >&2
+[[ -z ${DEBUG:-} ]] || echo "#CGROUPv${cgver} root_cpuset '${root_cpuset}', cg_cpuset '${cg_cpuset}', cg_mem '${cg_mem}', isolcpus '${isolcpus}'" >&2
 [[ -z ${isolcpus} ]] || echo "ISOLCPUS=\"${isolcpus}\""
