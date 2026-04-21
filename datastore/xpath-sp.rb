8d22ef9f0c616bf8ce7fb89938e3d1fc955815b7
Anton Todorov <a.todorov@storpool.com>
2026-04-21 15:42:16 +0300

    xpath-sp.rb: fix ruby lib location
    Change-Id: I7466038d52ed95692fc9d3d5184f190764f1e5f1

--- datastore/xpath-sp.rb
+++ datastore/xpath-sp.rb
@@ -20,6 +20,25 @@
 # Simple command to parse an XML document (b64 encoded) and return one or more
 # elements (refer by xpath)
 #
+
+ONE_LOCATION = ENV['ONE_LOCATION'] unless defined?(ONE_LOCATION)
+
+if !ONE_LOCATION
+    LIB_LOCATION      ||= '/usr/lib/one'
+    RUBY_LIB_LOCATION ||= '/usr/lib/one/ruby'
+    GEMS_LOCATION     ||= '/usr/share/one/gems'
+else
+    LIB_LOCATION      ||= ONE_LOCATION + '/lib'
+    RUBY_LIB_LOCATION ||= ONE_LOCATION + '/lib/ruby'
+    GEMS_LOCATION     ||= ONE_LOCATION + '/share/gems'
+end
+
+# %%RUBYGEMS_SETUP_BEGIN%%
+require 'load_opennebula_paths'
+# %%RUBYGEMS_SETUP_END%%
+
+$LOAD_PATH << RUBY_LIB_LOCATION
+
 require 'base64'
 require 'rexml/document'
 require 'getoptlong'
